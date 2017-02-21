41a59996736ce26fa724fa68aebc67370552a17f
Martin Page <wookoouk@gmail.com>
2017-02-21 11:42:54 +0000

    test

--- server.js
+++ server.js
@@ -1,76 +1,81 @@
-var express = require('express');
-var FB = require('fb');
-var moment = require('moment');
-var config = require('./config.json');
+const express = require('express');
+const FB = require('fb');
+var instaAPI = require('instagram-node').instagram();
+const moment = require('moment');
+const config = require('./config.json');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
+function getFBEvents() {
+    return new Promise((good, bad) => {
+        var events = {upcoming: [], past: []};
+
+
+        FB.api('oauth/access_token', {
+            client_id: config.fb_auth.client_id,
+            client_secret: config.fb_auth.client_secret,
+            grant_type: config.fb_auth.grant_type
+        }, function (fRes) {
+            if (!fRes || fRes.error) {
+                return bad(fRes.error);
+            } else {
+                const accessToken = fRes.access_token;
+                FB.setAccessToken(accessToken);
+                FB.api(
+                    "/" + config.FBID + "/events",
+                    function (response) {
+                        if (!response || response.err) {
+                            return bad(response.err);
+                        } else {
+                            const now = moment();
+
+                            response.data.map(function (value) {
+                                const eventDate = moment(value.start_time);
+
+                                value.month = eventDate.format('MMM');
+                                value.day = eventDate.format('D');
+
+                                const finished = now.isAfter(value.end_time);
+                                if (finished) {
+                                    events.past.push(value);
+                                } else {
+                                    events.upcoming.push(value);
+                                }
+                            });
+                            return good(events);
+                        }
+                    }
+                );
+            }
+        });
+    });
+}
 
 app.get('/', function (req, res) {
 
+    getFBEvents()
+        .then(events => {
 
-    var events = {upcoming: [], past: []};
+            instaAPI.use({
+                client_id: config.insta_auth.client_id,
+                client_secret: config.insta_auth.client_secret
+            });
 
+            instaAPI.user('2263249088', function (err, result, remaining, limit) {
 
-    FB.api('oauth/access_token', {
-        client_id: config.auth.client_id,
-        client_secret: config.auth.client_secret,
-        grant_type: config.auth.grant_type
-    }, function (fRes) {
-        if (!fRes || fRes.error) {
-            console.log(!fRes ? 'error occurred' : fRes.error);
-            return res.render('shows', {events: events});
-        } else {
-            var accessToken = fRes.access_token;
-            FB.setAccessToken(accessToken);
-            FB.api(
-                "/" + config.FBID + "/events",
-                function (response) {
-                    if (!response || response.err) {
-                        console.error(response.err)
-                    } else {
-                        var now = moment();
-
-                        response.data.map(function(value){
-                            var eventDate = moment(value.start_time);
-
-                            value.month = eventDate.format('MMM');
-                            value.day = eventDate.format('D');
-
-                            var finished = now.isAfter(value.end_time);
-                            if (finished) {
-                                events.past.push(value);
-                            } else {
-                                events.upcoming.push(value);
-                            }
-                        });
-
-                        return res.render('index', {events: events});
-
-                        // async.forEachOf(response.data, function (value, key, callback) {
-                        //     var eventDate = moment(value.start_time);
-                        //
-                        //     value.month = eventDate.format('MMM');
-                        //     value.day = eventDate.format('D');
-                        //
-                        //     var finished = now.isAfter(value.end_time);
-                        //     if (finished) {
-                        //         events.past.push(value);
-                        //     } else {
-                        //         events.upcoming.push(value);
-                        //     }
-                        //     callback();
-                        // }, function (err) {
-                        //     if (err) console.error(err.message);
-                        //     return res.render('index', {events: events});
-                        // });
-                    }
+                if(err){
+                    console.error(err)
+                } else {
+                    console.log(result);
                 }
-            );
-        }
-    });
+
+            });
+
+
+        })
+
 
 });
 
